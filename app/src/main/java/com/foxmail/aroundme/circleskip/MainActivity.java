8166b8156359b11d6a540fad9b284fa1819a0ac6
aroundone <1121068083@qq.com>
2018-12-17 17:38:05 +0800

    test

--- app/src/main/java/com/foxmail/aroundme/circleskip/MainActivity.java
+++ app/src/main/java/com/foxmail/aroundme/circleskip/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "click 3", Toast.LENGTH_SHORT).show();
-
             }
         });
 
